4dbeba1770ecf7cdb64a21d080f29b9b8e737393
Fernando Bustillos <fernando@Fernandos-MBP.fios-router.home>
2022-07-13 23:11:42 -0400

    HTML and CSS

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const selectors = {
+  container: document.querySelector("game"),
+  board: document.querySelector(".board"),
+  moves: document.querySelector(".moves"),
+  timer: document.querySelector(".timer"),
+  start: document.querySelector(".start"),
+  win: document.querySelector(".win"),
+};
